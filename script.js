d64dc1f38425ff611e023e4358dccf02d1da3695
Ivan Rodriguez <ivane.rodriguez02@gmail.com>
2022-10-13 23:43:49 -0300

    :sparkle: Feat: create first exercises to warm up

---
+++ script.js
@@ -0,0 +1,30 @@
+var name = 'Ivan';
+var age = 20;
+var single = false;
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+var colleagues = ['César', 'Esteban', 'Dai', 'Andrés', 'Ger']
+var values = {
+    name: 'Ivan',
+    age: 20,
+    single: false,
+    numbers: [1,2,3,4,5,6,7,8,9,10],
+    colleagues: ['César', 'Esteban', 'Dai', 'Andrés', 'Ger']
+}
+var user = {
+    name: 'César',
+    lastname: 'Ferreyra',
+    age: 27,
+    luckynumbers: [7,4,25],
+    pets: {
+        pet1: {
+            name: 'Charly',
+            dog_breed: 'Salchicha',
+            age: 4
+        },
+        pet2: {
+            name: 'Loli',
+            dog_breed: 'Pequines',
+            age: 10
+        }
+    }
+}
